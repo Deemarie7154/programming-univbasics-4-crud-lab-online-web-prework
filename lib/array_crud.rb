286e5e39ab1a17fc16a46b859bff224f473ae775
Dwight Marie <dwightmarie@gmail.com>
2019-08-25 16:41:23 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
  storage = [1,2,3,4]
-  claim_array_item = storage.
+  claim_array_item = storage.shift
 end
 
 def retrieve_element_from_index(array, index_number)
