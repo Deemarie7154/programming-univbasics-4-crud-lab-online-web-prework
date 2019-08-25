d2cc2b19b15658c27d64e161ab085d505e98c205
Dwight Marie <dwightmarie@gmail.com>
2019-08-25 16:38:08 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -12,7 +12,8 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  
+  torage = [1,2,3,4]
+  storage.push(0)
 end
 
 def remove_element_from_end_of_array(array)
